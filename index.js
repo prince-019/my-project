67e8731a9cce79659ca265645aa0083e199b6836
prince-o19 <pamela08101490576@gmail.com>
2023-11-22 04:56:10 -1100

    updated

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getElementById("searchButton");
-const results = document.getElementById("book");
-
-searchButton.addEventListener("click", () => {
-    const searchTerm = searchInput.value;
-
-    // Construct the API URL for searching books
-    const apiUrl = `https://openlibrary.org/search.json?q=${searchTerm}`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Display the search results
-            displayResults(data);
-            console.log(data)
-        })
-        .catch(error => {
-            console.log("Error:", error);
-            results.innerHTML = "An error occurred while fetching data.";
+ 
+        const lib = document.getElementById("library");
+        const fav = document.getElementById("Favourite");
+        const read1= document.getElementById("read1");
+        const read2 = document.getElementById("read2");
+        const pro = document.getElementById("profile");
+        
+        lib.addEventListener("click", function(){
+            window.location.href = "Library.html";
         });
-});
 
-function displayResults(data) {
-    results.innerHTML = ""; // Clear previous results
 
-    if (data.docs && data.docs.length > 0) {
-        data.docs.forEach(book => {
-            const title = book.title;
-            const author = book.author_name ? book.author_name[0] : "Author not available";
-            const year = book.first_publish_year ? book.first_publish_year : "Year not available";
-
-            const bookInfo = document.createElement("div");
-            bookInfo.innerHTML = `<h2>${title}</h2><p>Author: ${author}</p><p>Year: ${year}</p>`;
-            results.appendChild(bookInfo);
+        fav.addEventListener("click", function(){
+            window.location.href = "favourite.html";
         });
-    } else {
-        results.innerHTML = "No results found.";
-    }
+
+        read1.addEventListener("click", function(){
+            window.location.href = "Reading.html"
+        });
+        
+        read2.addEventListener("click", function(){
+            window.location.href = "Read.html"
+        });
+        
+
+        pro.addEventListener("click", function(){
+            window.location.href = "profile.html"
+        });
+
+        function profile(){
+            window.location.href = "profile.html";
+        }
+
+window.history.pushState(null, '', location.href);
+window.onpopstate = function () {
+  window.history.pushState(null, '', location.href);
+};
+ 
+function signout(){
+            window.location.href = "login.html";
+
+  window.history.pushState(null, '', location.href);
 }
